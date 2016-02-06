f6b820ee82439e9a3e756d81034569b4c0d247c6
John Behnke <John@aulacogen-32.dynamic2.rpi.edu>
2016-02-06 17:49:16 -0500

    Started the XCode Project

---
+++ RPI Tours/RPI ToursTests/RPI_ToursTests.swift
@@ -0,0 +1,36 @@
+//
+//  RPI_ToursTests.swift
+//  RPI ToursTests
+//
+//  Created by John Behnke on 2/6/16.
+//  Copyright © 2016 RPI Web Tech. All rights reserved.
+//
+
+import XCTest
+@testable import RPI_Tours
+
+class RPI_ToursTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
