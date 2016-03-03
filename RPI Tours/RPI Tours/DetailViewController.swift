1aff48d0b15d41ceadf3fdfed77d5a19b57860cf
JohnBehnke <behnkej@me.com>
2016-03-03 15:59:35 -0500

    Changed the .gitignore to hopefully work with API File

--- RPI Tours/RPI Tours/DetailViewController.swift
+++ RPI Tours/RPI Tours/DetailViewController.swift
@@ -8,26 +8,56 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
-class DetailViewController: UIViewController {
-
+class DetailViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var placesClient: GMSPlacesClient?
+    
     @IBOutlet weak var detailDescriptionLabel: UILabel!
     
     var placePicker: GMSPlacePicker?
     
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var addressLabel: UILabel!
-
-
+    
+    @IBOutlet weak var attributionTextView: UITextView!
+    
+    
     var detailItem: AnyObject? {
         didSet {
             // Update the view.
-            //self.configureView()
+            self.configureView()
         }
     }
     
-    @IBAction func pickPlace(sender: UIButton) {
-        let center = CLLocationCoordinate2DMake(42.73033677329484, -73.68044540970578)
+    
+    @IBAction func getCurrentPlace(sender: UIButton) {
+        
+        placesClient?.currentPlaceWithCallback({
+            (placeLikelihoodList: GMSPlaceLikelihoodList?, error: NSError?) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            self.nameLabel.text = "No current place"
+            self.addressLabel.text = ""
+            
+            if let placeLikelihoodList = placeLikelihoodList {
+                let place = placeLikelihoodList.likelihoods.first?.place
+                if let place = place {
+                    self.nameLabel.text = place.name
+                    self.addressLabel.text = place.formattedAddress!.componentsSeparatedByString(", ")
+                        .joinWithSeparator("\n")
+                }
+            }
+        })
+    }
+    
+    @IBAction func pickPlace(sender: UIBarButtonItem) {
+        let center = CLLocationCoordinate2DMake((locationManager.location?.coordinate.latitude)!, (locationManager.location?.coordinate.longitude)!)
         let northEast = CLLocationCoordinate2DMake(center.latitude + 0.001, center.longitude + 0.001)
         let southWest = CLLocationCoordinate2DMake(center.latitude - 0.001, center.longitude - 0.001)
         let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
@@ -41,15 +71,15 @@ class DetailViewController: UIViewController {
             }
             
             if let place = place {
-                self.nameLabel.text = place.name
-                self.addressLabel.text = place.formattedAddress!.componentsSeparatedByString(", ").joinWithSeparator("\n")
+                print("Place name \(place.name)")
+                print("Place address \(place.formattedAddress)")
+                print("Place attributions \(place.attributions)")
             } else {
-                self.nameLabel.text = "No place selected"
-                self.addressLabel.text = ""
+                print("No place selected")
             }
         })
     }
-
+    
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
@@ -58,28 +88,44 @@ class DetailViewController: UIViewController {
             }
         }
     }
-
+    
     override func viewDidLoad() {
-        let camera = GMSCameraPosition.cameraWithLatitude(42.73033677329484, longitude:-73.68044540970578, zoom: 18)
-        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        mapView.myLocationEnabled = true
-        self.view = mapView
         
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(42.73033677329484, -73.68044540970578)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            let camera = GMSCameraPosition.cameraWithLatitude((locationManager.location?.coordinate.latitude)!, longitude:(locationManager.location?.coordinate.longitude)!, zoom: 18)
+            let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+            mapView.settings.compassButton = true
+            mapView.settings.myLocationButton = true
+            mapView.myLocationEnabled = true
+            self.view = mapView
+            
+            
+        }
+        
+        
         super.viewDidLoad()
+        placesClient = GMSPlacesClient()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
