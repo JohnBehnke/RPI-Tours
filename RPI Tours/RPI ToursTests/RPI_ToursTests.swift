1283c417dc8b28dcb0a14211093ea5e26cfefcbc
David Ivey <iveyd@rpi.edu>
2016-04-10 19:28:34 -0400

    eyyyyyyy

--- RPI Tours/RPI ToursTests/RPI_ToursTests.swift
+++ RPI Tours/RPI ToursTests/RPI_ToursTests.swift
@@ -21,16 +21,21 @@ class RPI_ToursTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testjsonParser(){
+        let test = jsonParser()
+        XCTAssertTrue(test.getTours().count == 1)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//    
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
