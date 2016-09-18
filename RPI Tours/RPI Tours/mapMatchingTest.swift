606489963e049940eac3b32f2d1176baf7cf45e1
YOUR NAME <speicherjacob@gmail.com>
2016-09-18 19:35:00 -0400

    Updated to Swift 2.3🙌🏿

--- RPI Tours/RPI Tours/mapMatchingTest.swift
+++ RPI Tours/RPI Tours/mapMatchingTest.swift
@@ -1,61 +1,60 @@
+////
+////  mapMatchingTest.swift
+////  RPI Tours
+////
+////  Created by John Behnke on 6/12/16.
+////  Copyright © 2016 RPI Web Tech. All rights reserved.
+////
 //
-//  mapMatchingTest.swift
-//  RPI Tours
+//import Foundation
+//import SwiftyJSON
+////
+////
+////
+////let parameters = [
+////"foo": [1,2,3],
+////"bar": [
+////"baz": "qux"
+////]
+////]
+////
+////Alamofire.request(.POST, "https://httpbin.org/post", parameters: parameters, encoding: .JSON)
+//// HTTP body: {"foo": [1, 2, 3], "bar": {"baz": "qux"}}
 //
-//  Created by John Behnke on 6/12/16.
-//  Copyright © 2016 RPI Web Tech. All rights reserved.
-//
-
-import Foundation
-import Alamofire
-import SwiftyJSON
+////
+////curl -X POST \
+////--header "Content-Type:application/json" \
+////-d @trace.json \
+////"https://api.mapbox.com/matching/v4/mapbox.driving.json"
 //
 //
 //
-//let parameters = [
-//"foo": [1,2,3],
-//"bar": [
-//"baz": "qux"
-//]
-//]
 //
-//Alamofire.request(.POST, "https://httpbin.org/post", parameters: parameters, encoding: .JSON)
-// HTTP body: {"foo": [1, 2, 3], "bar": {"baz": "qux"}}
-
-//
-//curl -X POST \
-//--header "Content-Type:application/json" \
-//-d @trace.json \
-//"https://api.mapbox.com/matching/v4/mapbox.driving.json"
-
-
-
-
-func smoothPath(input: [[Double]]) -> [[Double]]{
-    let para = [
-        "type": "Feature",
-        "geometry": [
-            "type": "geojson",
-            "coordinates": input
-        ]
-    ]
-    
-    print(input)
-    var x = [AnyObject]()
-    Alamofire.request(.POST, "https://api.mapbox.com/matching/v5/mapbox.walking.json?access_token=\(mapBoxAPIKey)", parameters: para, encoding: .JSON).responseJSON { response in
-        print(response.request)  // original URL request
-       print(response.response) // URL response
-       print(response.data)     // server data
-        //print(response.result)   // result of response serialization
-        
-        if let value = response.result.value {
-            let json = JSON(value)
-            print(json)
-             x = json["features"][0]["properties"]["matchedPoints"].arrayObject!
-            
-            
-            //print("JSON: \(JSON["features"]!!["properties"]!!["matchedPoints"])")
-        }
-    }
-    return (x as? [[Double]])!
-}
+//func smoothPath(input: [[Double]]) -> [[Double]]{
+//    let para = [
+//        "type": "Feature",
+//        "geometry": [
+//            "type": "geojson",
+//            "coordinates": input
+//        ]
+//    ]
+//    
+//    print(input)
+//    var x = [AnyObject]()
+//    Alamofire.request(.POST, "https://api.mapbox.com/matching/v5/mapbox.walking.json?access_token=\(mapBoxAPIKey)", parameters: para, encoding: .JSON).responseJSON { response in
+//        print(response.request)  // original URL request
+//       print(response.response) // URL response
+//       print(response.data)     // server data
+//        //print(response.result)   // result of response serialization
+//        
+//        if let value = response.result.value {
+//            let json = JSON(value)
+//            print(json)
+//             x = json["features"][0]["properties"]["matchedPoints"].arrayObject!
+//            
+//            
+//            //print("JSON: \(JSON["features"]!!["properties"]!!["matchedPoints"])")
+//        }
+//    }
+//    return (x as? [[Double]])!
+//}
--- RPI Tours/RPI ToursUITests/RPI_ToursUITests.swift
+++ RPI Tours/RPI ToursUITests/RPI_ToursUITests.swift
@@ -1,85 +1,85 @@
+////
+////  RPI_ToursUITests.swift
+////  RPI ToursUITests
+////
+////  Created by John Behnke on 2/6/16.
+////  Copyright © 2016 RPI Web Tech. All rights reserved.
+////
 //
-//  RPI_ToursUITests.swift
-//  RPI ToursUITests
+//import XCTest
 //
-//  Created by John Behnke on 2/6/16.
-//  Copyright © 2016 RPI Web Tech. All rights reserved.
+//class RPI_ToursUITests: XCTestCase {
+//        
+//    override func setUp() {
+//        super.setUp()
+//        
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//        
+//        // In UI tests it is usually best to stop immediately when a failure occurs.
+//        continueAfterFailure = false
+//        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+//        XCUIApplication().launch()
 //
-
-import XCTest
-
-class RPI_ToursUITests: XCTestCase {
-        
-    override func setUp() {
-        super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    // This test case is to make sure that the map button works as intended
-    func testmapbutton(){
-        let app = XCUIApplication() //launch app
-        app.navigationBars["Tour Categories"].buttons["Map"].tap() //go to map
-        app.otherElements["Map"].tap() //move around the map to make sure everything renders
-        app.navigationBars["Map of Campus"].childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap() //back to main screen
-    }
-    
-    // This test case checks to make sure that a user can select a tour without issue
-    func testselecttour(){
-        let app = XCUIApplication() //launch app
-        let tablesQuery = app.tables //display category tables
-        tablesQuery.staticTexts["Miscellaneous Tours"].tap() //select a category
-        tablesQuery.staticTexts["Resa's Route"].tap() //select a tour
-        app.navigationBars["Master"].buttons["Miscellaneous Tours"].tap() //back to tours list in category
-        app.navigationBars.containingType(.StaticText, identifier:"Miscellaneous Tours").childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap() //back to categories
-    }
-    
-    //This test case is to make sure that the "take tour" button takes the user on a tour when tapped
-    func testtaketour(){
-        let app = XCUIApplication() //launch app
-        let tablesQuery = app.tables //display category tables
-        tablesQuery.staticTexts["Miscellaneous Tours"].tap() //select a category
-        tablesQuery.staticTexts["Resa's Route"].tap() //select a tour
-        tablesQuery.buttons["Start Tour"].tap() //start tour
-        let masterNavigationBar = app.navigationBars["Master"] //move the map to check for rendering issues
-        masterNavigationBar.childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap() //back to tour information page
-        masterNavigationBar.buttons["Miscellaneous Tours"].tap() //back to tours list in category
-        app.navigationBars.containingType(.StaticText, identifier:"Miscellaneous Tours").childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap() //back to categories
-    }
-    
-    //This test case is to make sure that the landmark pins display correctly
-    func testviewpin(){
-        let app = XCUIApplication() //launch app
-        app.navigationBars["Tour Categories"].buttons["Map"].tap() //go to map
-        app.otherElements["Map"].tap() //tap on a pin to see if correct information displays
-        app.navigationBars["Map of Campus"].childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap()//back to main screen
-    }
-    
-    //This test case is to make sure that the category information displays correctly
-    func testcategoryinfo(){
-        let app = XCUIApplication() //launch app
-        let tablesQuery = app.tables //display category
-        tablesQuery.buttons["More Info, General Tours"].tap() //tap the information button on the tour category
-        
-        let table = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).elementBoundByIndex(0).childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Table).element //create and display information window on top of table
-        table.tap() //exit window
-    }
-    
-}
+//        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+//    }
+//    
+//    override func tearDown() {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//        super.tearDown()
+//    }
+//    
+//    func testExample() {
+//        // Use recording to get started writing UI tests.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//    
+//    // This test case is to make sure that the map button works as intended
+//    func testmapbutton(){
+//        let app = XCUIApplication() //launch app
+//        app.navigationBars["Tour Categories"].buttons["Map"].tap() //go to map
+//        app.otherElements["Map"].tap() //move around the map to make sure everything renders
+//        app.navigationBars["Map of Campus"].childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap() //back to main screen
+//    }
+//    
+//    // This test case checks to make sure that a user can select a tour without issue
+//    func testselecttour(){
+//        let app = XCUIApplication() //launch app
+//        let tablesQuery = app.tables //display category tables
+//        tablesQuery.staticTexts["Miscellaneous Tours"].tap() //select a category
+//        tablesQuery.staticTexts["Resa's Route"].tap() //select a tour
+//        app.navigationBars["Master"].buttons["Miscellaneous Tours"].tap() //back to tours list in category
+//        app.navigationBars.containingType(.StaticText, identifier:"Miscellaneous Tours").childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap() //back to categories
+//    }
+//    
+//    //This test case is to make sure that the "take tour" button takes the user on a tour when tapped
+//    func testtaketour(){
+//        let app = XCUIApplication() //launch app
+//        let tablesQuery = app.tables //display category tables
+//        tablesQuery.staticTexts["Miscellaneous Tours"].tap() //select a category
+//        tablesQuery.staticTexts["Resa's Route"].tap() //select a tour
+//        tablesQuery.buttons["Start Tour"].tap() //start tour
+//        let masterNavigationBar = app.navigationBars["Master"] //move the map to check for rendering issues
+//        masterNavigationBar.childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap() //back to tour information page
+//        masterNavigationBar.buttons["Miscellaneous Tours"].tap() //back to tours list in category
+//        app.navigationBars.containingType(.StaticText, identifier:"Miscellaneous Tours").childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap() //back to categories
+//    }
+//    
+//    //This test case is to make sure that the landmark pins display correctly
+//    func testviewpin(){
+//        let app = XCUIApplication() //launch app
+//        app.navigationBars["Tour Categories"].buttons["Map"].tap() //go to map
+//        app.otherElements["Map"].tap() //tap on a pin to see if correct information displays
+//        app.navigationBars["Map of Campus"].childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap()//back to main screen
+//    }
+//    
+//    //This test case is to make sure that the category information displays correctly
+//    func testcategoryinfo(){
+//        let app = XCUIApplication() //launch app
+//        let tablesQuery = app.tables //display category
+//        tablesQuery.buttons["More Info, General Tours"].tap() //tap the information button on the tour category
+//        
+//        let table = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).elementBoundByIndex(0).childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Table).element //create and display information window on top of table
+//        table.tap() //exit window
+//    }
+//    
+//}
