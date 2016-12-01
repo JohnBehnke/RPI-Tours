2fcc68f226b5ba97854783f885d1b386d3d95e2f
burtonwilliamt <burtonwilliamt@gmail.com>
2016-11-30 19:25:26 -0500

    Does this async call work?

--- RPI Tours/RPI Tours/InfoViewController.swift
+++ RPI Tours/RPI Tours/InfoViewController.swift
@@ -12,35 +12,38 @@ import CoreLocation
 import TNImageSliderViewController
 
 class InfoViewController: UITableViewController {
-    
+
     //MARK: IBOutlets
     @IBOutlet var landmarkDescriptionLabel: UILabel!
     @IBOutlet var descriptionCell: UITableViewCell!
-    
+
     //MARK: Global Variables
     var landmarkName:String = ""
     var landmarkDesc:String = ""
     var landmarkInformation: [Landmark] = []
     var imageSliderVC: TNImageSliderViewController!
-    
+
     //MARK: Segues
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "imageSlider" {
             imageSliderVC = segue.destinationViewController as! TNImageSliderViewController
         }
     }
-    
+
     //MARK: System Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = self.landmarkName
-        
+
         let chosenLandmark = searchForLandmark()
-        
+
         self.landmarkDescriptionLabel.text = chosenLandmark.getDesc()
-        
-        imageSliderVC.images = chosenLandmark.getImages()
-        
+
+        //Does this async call work?
+        dispatch_async(dispatch_get_global_queue(Int(QOS_CLASS_USER_INITIATED.value), 0)) {
+          imageSliderVC.images = chosenLandmark.getImages()
+        }
+
         //set the imageSlider options
         var options = TNImageSliderViewOptions()
         options.pageControlHidden = false
@@ -48,14 +51,14 @@ class InfoViewController: UITableViewController {
         options.shouldStartFromBeginning = true
         options.imageContentMode = .ScaleAspectFit
         options.pageControlCurrentIndicatorTintColor = UIColor.redColor()
-        
+
         imageSliderVC.options = options
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
     //MARK: Helper Functions
     func searchForLandmark() -> Landmark {
         for landmark in landmarkInformation {
@@ -63,29 +66,29 @@ class InfoViewController: UITableViewController {
                 return landmark
             }
         }
-        
+
         let blankLandmark = Landmark(name: "Unknown Landmark", desc: "I'm sorry, there is no information yet for this landmark.", lat: 0.0, long: 0.0)
         blankLandmark.setImages(["https://c1.staticflickr.com/5/4034/4544827697_6f73866999_b.jpg"])
-        
+
         return blankLandmark
     }
-    
+
     //MARK: TableView Functions
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        
+
         if indexPath.section == 0 {
             return UITableViewAutomaticDimension
         }
-        
+
         return 300
     }
-    
+
     override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        
+
         if indexPath.section == 0 {
             return UITableViewAutomaticDimension
         }
-        
+
         return 300
     }
 }
